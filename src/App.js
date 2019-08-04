cb5d11d2315f9ca3d01212cb1bb70481a9e29779
Dan Lubbers <lubbers.dan@gmail.com>
2019-08-03 21:24:21 -0400

    Getting Links/Routes setup with Nav and Hero Component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
+import Navbar from './components/Navbar';
+
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <>
-    <Home />
-    <Rooms />
-    <SingleRoom />
-    <Error />
+      {/* Placing the Navbar outside of the Switch allows it to be accessed across all pages */}
+      <Navbar />
+      {/* Switch is placed so if no route matches a component, the page that will be displayed is going to be the Route without a path, specifically the Error Component */}
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/rooms/" component={Rooms}/>
+        <Route exact path="/rooms/:slug" component={SingleRoom}/>
+        <Route component={Error}/> 
+      </Switch>
+    
     </>
   )
 }
