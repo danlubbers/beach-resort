71c7d75fb8d8084d40eafd97e6a2ce99cbb4dba6
Dan Lubbers <lubbers.dan@gmail.com>
2019-08-05 13:22:59 -0400

    Setup FeaturedRooms, Room and Loading components

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../context';
+import Loading from "./Loading";
+import Room from './Room';
+import Title from './Title';
 
 export default class FeaturedRooms extends Component {
 
@@ -7,12 +10,21 @@ export default class FeaturedRooms extends Component {
 
     render() {
         // Destructure off the context to get the object off state in context.js
-        const {name, greeting} = this.context;
-    
+        // const {name, greeting} = this.context;
+        let { loading, featuredRooms: rooms} = this.context;
+        console.log(rooms)
+        rooms = rooms.map(room => {
+            return <Room key={room.id} room={room} />
+        })
+
         return (
-            <div>
-                Featured Rooms {greeting} {name}
-            </div>
+            <section className="featured-rooms">
+                {/* Featured Rooms {greeting} {name} */}
+                <Title title="Featured Rooms"/>
+                <div className="featured-rooms-center">
+                    {loading ? <Loading /> : rooms}
+                </div>
+            </section>
         )
     }
 }
--- src/context.js
+++ src/context.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import items from './data';
 // This is to use the Context API in React
 // Access to Provider and Consumer
 const RoomContext = React.createContext();
@@ -7,9 +7,39 @@ const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
     state = {
-        greeting: 'Hello',
-        name: 'John'
+        // This was dummy data to test
+        // greeting: 'Hello',
+        // name: 'John'
+        rooms: [],
+        sortedRooms: [],
+        featuredRooms: [],
+        loading: true
     }
+
+    // getData
+
+    componentDidMount() {
+        // this.getData
+        let rooms = this.formatData(items);
+        console.log(rooms)
+        let featuredRooms = rooms.filter(room => room.featured === true);
+        console.log(featuredRooms)
+        this.setState({
+            rooms, featuredRooms, sortedRooms: rooms, loading: false
+        })
+    }
+
+    formatData(items) {
+        let tempItems = items.map(item => {
+          let id = item.sys.id;
+          let images = item.fields.images.map(image => image.fields.file.url);
+
+          let room = {...item.fields, images, id };
+          return room;
+        })
+        return tempItems;
+    }
+
 
     render() {
         return (
